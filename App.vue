c67aa421ae66837c8effedec6d563f9bebb63d2d
Eleshghkooh <156024685+Eleshghkooh@users.noreply.github.com>
2024-08-18 11:07:55 +0200

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,23 @@
+<!-- the script is where the js code goes -->
+<script setup>
+import { ref } from "vue" // you need this to use ref()
+
+
+</script>
+
+
+
+<!-- the template is where the html code goes -->
+<template>
+
+
+
+</template>
+
+
+
+<!-- style is where the css code goes -->
+<style scoped>
+
+
+</style>
