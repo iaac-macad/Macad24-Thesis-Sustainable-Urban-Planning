ba04ea8d534b93b284a60acf98deb82cbfd40cc8
Eleshghkooh <elnaz.eshghkooh@students.iaac.net>
2024-08-18 13:20:55 +0200

    add styles2

---
+++ Src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h1>Homework Component</h1>
+      <toggle-input v-model="toggleValue"></toggle-input>
+      <dropdown-input v-model="selectedOption" :options="options"></dropdown-input>
+    </div>
+  </template>
+  
+  <script>
+  import ToggleInput from './ToggleInput.vue';
+  import DropdownInput from './DropdownInput.vue';
+  
+  export default {
+    components: {
+      ToggleInput,
+      DropdownInput
+    },
+    data() {
+      return {
+        toggleValue: false,
+        selectedOption: null,
+        options: ['Option 1', 'Option 2', 'Option 3']
+      };
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
